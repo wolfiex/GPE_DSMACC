49be1aa9192433c9cd118da6f18a41ca4cbae552
Daniel Ellis <daniel.ellis@york.ac.uk>
2017-04-25 18:19:15 +0100

    lhs julia ics maker

--- createruns.jl
+++ createruns.jl
@@ -3,20 +3,27 @@ R"library(lhs)"
 len = length
 
 
+rows = 10
+
+
 constrain= readtable("constrained_ics.csv",separator = ',')
 changing = readtable("changing_ics.csv",separator = ',')
-
-rows = 10
 columns = len(changing[1])
-
-[constrain[i]=constrain[2] for i in 1:rows]
-
-
+[constrain[Symbol("x$i")] = constrain[:x1] for i in 1:rows]
 R" lhsgrid = optimumLHS(n=$(columns), k=$(rows), maxSweeps=20, eps=.1, verbose=FALSE)"
 @rget lhsgrid
+#concat df vcat or hcat  # .operation = appply to all elements 
+lhsgrid =  ((changing[:max]-changing[:min]) .* lhsgrid) .+ changing[:min]
+lhsgrid =  DataFrame(Array(hcat( changing[:species],lhsgrid)))
+indexarray = [Symbol("x$(i)") for i in 0:rows]
+indexarray[1] = "species" 
+names!(lhsgrid,indexarray)
+ics = vcat(constrain,lhsgrid)
 
-while condition
-  body
-end
+writetable("latin.csv", ics, separator = ',', header = false,quotemark=' ')
 
 #changing[:species]
+print("ahhh",ics)
+
+
+
